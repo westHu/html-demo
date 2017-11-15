ed27f0f11e2634dc0db60b62684ff565d77a5d10
hpj <west_hangzhou@163.com>
2017-11-15 19:35:04 +0800

    3

---
+++ 水雨情/data/dbfx.js
@@ -0,0 +1,68 @@
+var dataString ='<chart canvasPadding="10" caption="同时间段不同点位对比" baseFontSize="12"  showValues="0" yAxisName="流量(m³/H)" bgColor="F7F7F7, E9E9E9" numVDivLines="10" divLineAlpha="30"  labelPadding ="10" yAxisValuesPadding ="10" showValues="1" rotateValues="1" valuePosition="auto">\n\
+	<categories>\n\
+		<category label="11:00" /> \n\
+		<category label="11:05" /> \n\
+		<category label="11:10" /> \n\
+		<category label="11:15" /> \n\
+		<category label="11:20" /> \n\
+		<category label="11:25" /> \n\
+		<category label="11:30" /> \n\
+		<category label="11:35" /> \n\
+		<category label="11:40" /> \n\
+		<category label="11:45" /> \n\
+		<category label="11:50" /> \n\
+		<category label="11:55" /> \n\
+		<category label="12:00" /> \n\
+	</categories>\n\
+\n\
+	<dataset seriesName="太沙河原水-出水瞬时流量" color="00ced1" >\n\
+		<set value="6344" /> \n\
+		<set value="6566" /> \n\
+		<set value="7789" /> \n\
+		<set value="7755" valuePosition="above"/> \n\
+		<set value="7899" /> \n\
+		<set value="6900" /> \n\
+		<set value="7011" /> \n\
+		<set value="6122" /> \n\
+		<set value="7433" /> \n\
+		<set value="6544" /> \n\
+		<set value="7655" /> \n\
+		<set value="6789" /> \n\
+		<set value="6600" /> \n\
+	</dataset>\n\
+\n\
+	<dataset seriesName="登沙河净水-出水瞬时流量" color="875652">\n\
+			<set value="4121" /> \n\
+		<set value="3588" /> \n\
+		<set value="3699" /> \n\
+		<set value="3899" valuePosition="above"/> \n\
+		<set value="3011" /> \n\
+		<set value="3288" /> \n\
+		<set value="3455" /> \n\
+		<set value="3788" /> \n\
+		<set value="3900" /> \n\
+		<set value="3250" /> \n\
+		<set value="3450" /> \n\
+		<set value="3550" /> \n\
+		<set value="3880" /> \n\
+		<set value="3600" /> \n\
+	</dataset>\n\
+\n\
+\n\
+	<dataset seriesName="青山净水-出水瞬时流量" color="F6BD0F">\n\
+		<set value="4789" /> \n\
+		<set value="4890" /> \n\
+		<set value="4456" /> \n\
+		<set value="4211" valuePosition="above"/> \n\
+		<set value="4255" /> \n\
+		<set value="4644" /> \n\
+		<set value="4555" /> \n\
+		<set value="4655" /> \n\
+		<set value="4555" /> \n\
+		<set value="4655" /> \n\
+		<set value="4555" /> \n\
+		<set value="4655" /> \n\
+		<set value="4600" /> \n\
+	</dataset>\n\
+\n\
+</chart>';
